8c6ecc1c14a4018e99765d5b60bb7c0933d48d1a
Neil Massey <neil.massey@stfc.ac.uk>
2022-07-14 16:26:54 +0100

    Fixed fragment not being returned

--- src/cfa_var.c
+++ src/cfa_var.c
@@ -795,6 +795,7 @@ extern int cfa_var_get1_frag(const int cfa_id, const int cfa_var_id,
                 cfa_err = cfa_netcdf_read1_frag(agg_cont->x_id, cfa_id, 
                                                 cfa_var_id, frag);
                 CFA_CHECK(cfa_err);
+                *ret_frag = frag;
             break;
             case CFA_UNKNOWN:
             default:
