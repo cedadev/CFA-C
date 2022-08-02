e8c27615ab1925029ea73c743666fff1a0fcbecd
IVlaD17 <vlad.ionica@stfc.ac.uk>
2022-08-02 14:18:48 +0100

    Separated the types into their own header file

--- src/cfa.h
+++ src/cfa.h
@@ -6,23 +6,7 @@
 
 #include "cfa_mem.h"
 #include "cfa_errs.h"
-
-/* Type constants - these match the netCDF ones, except for strings
-   Currently we are only supporting numeric types
-*/
-#define CFA_NAT          (0)      /**< Not A Type */
-#define CFA_BYTE         (1)      /**< signed 1 byte integer */
-#define CFA_CHAR         (2)      /**< ISO/ASCII character */
-#define CFA_SHORT        (3)      /**< signed 2 byte integer */
-#define CFA_INT          (4)      /**< signed 4 byte integer */
-#define CFA_LONG         (CFA_INT)
-#define CFA_FLOAT        (5)      /**< single precision floating point number */
-#define CFA_DOUBLE       (6)      /**< double precision floating point number */
-#define CFA_UBYTE        (7)      /**< unsigned 1 byte int */
-#define CFA_USHORT       (8)      /**< unsigned 2-byte int */
-#define CFA_UINT         (9)      /**< unsigned 4-byte int */
-#define CFA_INT64        (10)     /**< signed 8-byte int */
-#define CFA_UINT64       (11)     /**< unsigned 8-byte int */
+#include "cfa_types.h"
 
 /* CFA metadata identifier string */
 #define CFA_CONVENTION ("CFA-")
